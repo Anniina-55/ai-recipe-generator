74f98135d2916438b9430d4d6fe85b46d7ec5765
Anniina-55 <anniina.leikas55@gmail.com>
2025-09-12 01:10:43 +0300

    Update amplify.yml to include backend dir and adjust App/main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,10 @@ import { generateClient } from "aws-amplify/data";
 import outputs from "./amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
-
 Amplify.configure(outputs);
 const amplifyClient = generateClient<Schema>({
 authMode: "userPool",
 });
-
 function App() {
 const [result, setResult] = useState<string>("");
 const [loading, setLoading] = useState(false);
@@ -37,48 +35,48 @@ setLoading(false);
 }
 };
 return (
-    <div className="app-container">
-    <div className="header-container">
-    <h1 className="main-header">
-    Meet Your Personal
-    <br />
-    <span className="highlight">Recipe AI</span>
-    </h1>
-    <p className="description">
-    Simply type a few ingredients using the format ingredient1,
-    ingredient2, etc., and Recipe AI will generate an all-new
-    recipe on
-    demand...
-    </p>
-    </div>
-    <form onSubmit={onSubmit} className="form-container">
-    <div className="search-container">
-    <input
-    type="text"
-    className="wide-input"
-    id="ingredients"
-    name="ingredients"
-    placeholder="Ingredient1, Ingredient2, Ingredient3,...etc"
-    />
-    <button type="submit" className="search-button">
-    Generate
-    </button>
-    </div>
-    </form>
-    <div className="result-container">
-    {loading ? (
-    <div className="loader-container">
-    <p>Loading...</p>
-    <Loader size="large" />
-    <Placeholder size="large" />
-    <Placeholder size="large" />
-    <Placeholder size="large" />
-    </div>
-    ) : (
-    result && <p className="result">{result}</p>
-    )}
-    </div>
-    </div>
+<div className="app-container">
+<div className="header-container">
+<h1 className="main-header">
+Meet Your Personal
+<br />
+<span className="highlight">Recipe AI</span>
+</h1>
+<p className="description">
+Simply type a few ingredients using the format ingredient1,
+ingredient2, etc., and Recipe AI will generate an all-new
+recipe on
+demand...
+</p>
+</div>
+<form onSubmit={onSubmit} className="form-container">
+<div className="search-container">
+<input
+type="text"
+className="wide-input"
+id="ingredients"
+name="ingredients"
+placeholder="Ingredient1, Ingredient2, Ingredient3,...etc"
+/>
+<button type="submit" className="search-button">
+Generate
+</button>
+</div>
+</form>
+<div className="result-container">
+{loading ? (
+<div className="loader-container">
+<p>Loading...</p>
+<Loader size="large" />
+<Placeholder size="large" />
+<Placeholder size="large" />
+<Placeholder size="large" />
+</div>
+) : (
+result && <p className="result">{result}</p>
+)}
+</div>
+</div>
 );
 }
 export default App
